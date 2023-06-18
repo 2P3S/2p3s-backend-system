7f3ff0717f9177b4bcd19202f6f0bed8044f1835
JeongJaeSoon <wjdwotns1006@gmail.com>
2023-06-19 00:03:31 +0900

    fix: [#14] Room 생성 시 필요한 deckType 추가

--- src/scrum-dice/dto/room/create-room.dto.ts
+++ src/scrum-dice/dto/room/create-room.dto.ts
@@ -1,7 +1,14 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+import { Deck } from 'src/scrum-dice/entities/room.entities';
 
 export class CreateRoomDto {
   @IsNotEmpty()
   @IsString()
   readonly roomName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(['FIBONACCI_NUMBERS', 'MODIFIED_FIBONACCI_NUMBERS'])
+  readonly deckType: Deck;
 }
