61ffcd493ee5ce9470ea1afaf564f3a3301f4eba
JeongJaeSoon <wjdwotns1006@gmail.com>
2023-08-07 00:35:58 +0900

    chore: 타입 이름 수정

--- src/scrum-dice/entities/card.entities.ts
+++ src/scrum-dice/entities/card.entities.ts
@@ -1,29 +1,21 @@
-export const types = ['cost-type', 'not-cost-type'] as const;
-export type Type = (typeof types)[number];
-
-export const costContents = [
-  '0',
-  '0.5',
-  '1',
-  '2',
-  '3',
-  '5',
-  '8',
-  '13',
-  '20',
-  '21',
-  '34',
-  '40',
-  '55',
-  '89',
-  '100',
-] as const;
-export type CostContent = (typeof costContents)[number];
-
-export const notCostContents = ['coffee', 'question', 'king', 'break'] as const;
-export type NotCostContent = (typeof notCostContents)[number];
-
+export type Type = 'cost-type' | 'not-cost-type';
+export type CostContent =
+  | '0'
+  | '0.5'
+  | '1'
+  | '2'
+  | '3'
+  | '5'
+  | '8'
+  | '13'
+  | '20'
+  | '21'
+  | '34'
+  | '40'
+  | '55'
+  | '89'
+  | '100';
+export type NotCostContent = 'coffee' | 'question' | 'king' | 'break';
 export type Content = CostContent | NotCostContent;
-
 export type Card = { type: Type; content: Content };
 export type Cards = { [memberId: string]: Card };
